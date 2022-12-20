2859f3ba6cb3963d7484a28881826910698b8e92
LarysaHliieva <lora.glievaya@gmail.com>
2022-12-21 00:52:20 +0200

    added logics search countries

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+import { fetchCountries } from './fetchCountries';
+
 import './css/styles.css';
 
 const DEBOUNCE_DELAY = 300;
+
+const refs = {
+  searchInput: document.querySelector('#search-box'),
+  countryList: document.querySelector('.country-list'),
+  countryInfo: document.querySelector('.country-info'),
+};
+
+let searchQuery = '';
+
+refs.searchInput.addEventListener('input', debounce(onSearch, 300));
+
+function onSearch(e) {
+  searchQuery = e.target.value;
+  fetchCountries(searchQuery).then(data => console.log(data));
+}
+
+// Notify.failure('Qui timide rogat docet negare');
+// Notify.info('Cogito ergo sum');
